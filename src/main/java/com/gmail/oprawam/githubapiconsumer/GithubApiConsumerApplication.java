2d883bc4179cb3e3498a6038266e2826b03ed433
mawarop <oprawam@gmail.com>
2023-09-02 17:02:16 +0200

    Init

---
+++ src/main/java/com/gmail/oprawam/githubapiconsumer/GithubApiConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.oprawam.githubapiconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubApiConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubApiConsumerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gmail/oprawam/githubapiconsumer/GithubApiConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.oprawam.githubapiconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubApiConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
