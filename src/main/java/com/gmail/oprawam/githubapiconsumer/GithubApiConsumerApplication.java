255eb890c129aa6a897b59acaaae16a2f22680fb
mawarop <oprawam@gmail.com>
2023-09-24 21:12:28 +0200

    test: BlockHound

--- src/main/java/com/gmail/oprawam/githubapiconsumer/GithubApiConsumerApplication.java
+++ src/main/java/com/gmail/oprawam/githubapiconsumer/GithubApiConsumerApplication.java
@@ -2,11 +2,14 @@ package com.gmail.oprawam.githubapiconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class GithubApiConsumerApplication {
 
     public static void main(String[] args) {
+        BlockHound.install();
+
         SpringApplication.run(GithubApiConsumerApplication.class, args);
     }
 
